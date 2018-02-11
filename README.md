56d40a8157f1b997753430e6498ac983662dd3a7
Darren Klein <darren.a.klein@gmail.com>
2018-02-11 16:48:15 -0500

    Update formatting script and README

--- README.md
+++ README.md
@@ -5,4 +5,46 @@ Git-to-JSON is a simple NPM module designed to retrieve the info for your projec
 
 ## Installation
 
+Install this package by running
+
+``` SH
+npm install --save darrenklein/git-to-json
+```
+
+or update your `package.json` dependencies like so:
+
+``` JSON
+"dependencies": {
+	"git-to-json": "git+https://git@github.com/darrenklein/git-to-json.git"
+}
+```
+
+and run
+
+``` SH
+npm install
+```
+
 ## Usage
+
+Git-to-JSON will read the info from your most recent git commit and write it to the file `gitignore/git-commit-info.js` in JSON format. If a `gitignore/` directory does not yet exist, Git-to-JSON will create it.
+
+Example `gitignore/git-commit-info.js`:
+
+``` JS
+module.exports = {"commit":"5aqfaa6741871zz5e67683a012a0062bdbd46a8z","author":"Foo Bar <foo@bar.com>","date":"Sun Feb 11 13:37:29 2018 -0500","message":"My cool git commit"}
+```
+
+To run Git-to-JSON, simply include the following command in the NPM script of your choice:
+
+``` JS
+git-to-json
+```
+
+for example, in your project's `package.json` file:
+
+``` JS
+"scripts": {
+	"start": "git-to-json && node index.js"
+}
+```
--- bin/git-to-json.js
+++ bin/git-to-json.js
@@ -1,25 +1,30 @@
 #! /usr/bin/env node
 
 const fs = require("fs"),
-	exportPrefix = "module.exports =",
 	dir = "./gitignore/",
-	fileName = "git-commit-info.js"
+	fileName = "git-commit-info.js",
+	formatAttribute = (attribute) => {
+		if (Array.isArray(attribute)) {
+			return attribute.toString().trim()
+		}
+
+		return attribute.indexOf(":") > -1 ? attribute.split(/:(.+)/)[1].trim() : attribute.split(" ")[1]
+	},
+	stdoutToJSON = (stdout) => {
+		const splitStdout = stdout.split(/\n/),
+			stdoutJSON = {
+				"commit": formatAttribute(splitStdout[0]),
+				"author": formatAttribute(splitStdout[1]),
+				"date": formatAttribute(splitStdout[2]),
+				"message": formatAttribute(splitStdout.slice(4, splitStdout.length - 1))
+			}
+
+		return JSON.stringify(stdoutJSON)
+	}
 
 if (!fs.existsSync(dir)) {
 	fs.mkdirSync(dir)
 	console.log(`Created ${dir}`)
-}
-
-const stdoutToJSON = (stdout) => {
-	const splitStdout = stdout.split(/\n/),
-		stdoutJSON = {
-			"commit": splitStdout[0],
-			"author": splitStdout[1],
-			"date": splitStdout[2],
-			"message": splitStdout.slice(4, splitStdout.length - 1).toString().trim()
-		}
-
-	return JSON.stringify(stdoutJSON)
 }
 
 require("child_process").exec("git log -1", (err, stdout) => {
@@ -28,13 +33,9 @@ require("child_process").exec("git log -1", (err, stdout) => {
 	}
 
 	const stdoutJSON = stdoutToJSON(stdout),
-		commitOutput = `${exportPrefix} ${stdoutJSON}`
+		commitOutput = `module.exports = ${stdoutJSON}`
 
 	return fs.writeFile(`${dir}${fileName}`, commitOutput, (error) => {
-		if (error) {
-			return console.log(error)
-		}
-
-		return console.log(`Git commit info saved to ${dir}${fileName}: ${stdoutJSON}`)
+		return error ? console.log(error) : console.log(`Git commit info saved to ${dir}${fileName}: ${stdoutJSON}`)
 	})
 })
